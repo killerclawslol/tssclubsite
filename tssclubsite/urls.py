1b71e8e65210b670a6c3eaa2297d6f92c240b15b
killerclawslol <40039203+killerclawslol@users.noreply.github.com>
2018-11-16 19:22:55 -0500

    Update urls.py

--- tssclubsite/urls.py
+++ tssclubsite/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls import url
 
 urlpatterns = [
     path('club_management/', include('club_management.urls')),
     path('admin/', admin.site.urls),
+    path('', include('clubsite.urls')),
+    url(r"^$", include('clubsite.urls')),
 ]
